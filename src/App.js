e53f88a849f4b50a3fd212881e379ed5c5ab48fc
rajitha690 <rajitharoyal85@gmail.com>
2025-04-17 15:50:42 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '40px' }}>
+      <h1>Hello from React + Docker! 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
