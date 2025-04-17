f07ec131c5822ed47b15e8fb7fa5e65424cf0ad8
rajitha690 <rajitharoyal85@gmail.com>
2025-04-17 16:17:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '40px' }}>
-      <h1>Hello from React + Docker! 🚀</h1>
+      <h1>Hello from React frontend! 🚀</h1>
     </div>
   );
 }
